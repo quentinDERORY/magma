395f1aff24a3929af0bb1b46b1b95b850e8108ee
quentinDERORY <15911421+quentinDERORY@users.noreply.github.com>
2021-09-07 16:55:58 -0700

    fix ifned
    Signed-off-by: quentinDERORY <15911421+quentinDERORY@users.noreply.github.com>

--- orc8r/gateway/python/python.mk
+++ orc8r/gateway/python/python.mk
@@ -37,9 +37,9 @@ install_virtualenv:
 	@echo "Initializing virtualenv with python version $(PYTHON_VERSION)"
 	virtualenv -v  --no-pip --no-setuptools --no-wheel --system-site-packages --python=/usr/bin/python$(PYTHON_VERSION) $(PYTHON_BUILD)
 	. $(PYTHON_BUILD)/bin/activate;
-	ifndef INTEG_TESTS
-	    $(VIRT_ENV_PIP_INSTALL) "pip>=20.3.2"
-    endif
+ifndef INTEG_TESTS
+    $(VIRT_ENV_PIP_INSTALL) "pip>=20.3.2"
+endif
 
 setupenv: $(PYTHON_BUILD)/sysdeps $(SITE_PACKAGES_DIR)/setuptools
 
@@ -53,9 +53,9 @@ $(PYTHON_BUILD):
 	mkdir -p $(PYTHON_BUILD)
 
 $(SITE_PACKAGES_DIR)/setuptools: install_virtualenv
-    ifndef INTEG_TESTS
-	    $(VIRT_ENV_PIP_INSTALL) "setuptools==49.6.0"  # newer than 41.0.1
-	endif
+ifndef INTEG_TESTS
+    $(VIRT_ENV_PIP_INSTALL) "setuptools==49.6.0"  # newer than 41.0.1
+endif
 
 py_patches:
 	patch --dry-run -N -s -f $(SITE_PACKAGES_DIR)/aioeventlet.py <$(PATCHES_DIR)/aioeventlet.py38.patch 2>/dev/null \
@@ -70,9 +70,9 @@ py_patches:
 	&&  (patch -N -s -f $(SITE_PACKAGES_DIR)/ryu/ofproto/nx_actions.py <$(PATCHES_DIR)/0001-Set-unknown-dpid-ofctl-log-to-debug.patch && echo "ryu was patched" ) \
 	|| ( true && echo "skipping ryu patch since it was already applied")
 
-    ifndef INTEG_TESTS
-	    $(VIRT_ENV_PIP_INSTALL) --force-reinstall git+https://github.com/URenko/aioh2.git
-	endif
+ifndef INTEG_TESTS
+    $(VIRT_ENV_PIP_INSTALL) --force-reinstall git+https://github.com/URenko/aioh2.git
+endif
 
 swagger:: swagger_prereqs $(SWAGGER_LIST)
 swagger_prereqs:
@@ -118,9 +118,9 @@ prometheus_proto:
 
 # If you update the version here, you probably also want to update it in setup.py
 $(BIN)/grpcio-tools: install_virtualenv
-    ifndef INTEG_TESTS
-	    $(VIRT_ENV_PIP_INSTALL) "grpcio-tools>=1.16.1"
-	endif
+ifndef INTEG_TESTS
+    $(VIRT_ENV_PIP_INSTALL) "grpcio-tools>=1.16.1"
+endif
 
 .test: .tests .sudo_tests
 
